e0983f4d7926912162ec727ce6a5542b7fbdf8b0
Kamil Kozak <130604@student.uni.opole.pl>
2022-04-29 08:19:27 +0200

    Początek - wątki

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     //Ustawienia Ekranu
     final int orgTileSize = 16;  //16x16
     final int scale = 3;
@@ -12,9 +12,21 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenColumn;  //768 pixels
     final int screenHeight = tileSize * maxScreenRow;    //576 pixels
 
+    Thread gameThread;
+
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
     }
 }
